9be38f8160ed7bb19b3989498bebe8611eaac618
gnu <41645782+NevinJulian@users.noreply.github.com>
2025-06-05 17:05:04 +0200

    work on firebase deployment

--- scripts/02_scrape_europapark.sh
+++ scripts/02_scrape_europapark.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./01_queue-times-scrape.py --park-id 51 --login-email <user_account_email> --login-password='<user_account_password>' --start-date 2017-05-22 --end-date 2024-12-31
+./01_queue-times-scrape.py --park-id 51 --login-email nevinhelfenstein@gmail.com --login-password='cQAk5dkrm7A!zeP' --start-date 2025-05-19 --end-date 2025-06-05
+
+    ./04_gather_weather_data.py
